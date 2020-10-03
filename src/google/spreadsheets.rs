e46e67a29353ea14d5918e5f6c97157461d7a4e0
ssankko <literaamr@gmail.com>
2020-10-03 23:41:28 +1000

    Spreadsheets fixes

--- src/google/spreadsheets.rs
+++ src/google/spreadsheets.rs
@@ -1,21 +1,51 @@
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
 
-crate::rest_service!("sheets", "https://www.googleapis.com/auth/spreadsheets");
+use crate::google::auth;
+use once_cell::sync::OnceCell;
+use reqwest::Client;
+use yup_oauth2::authenticator::DefaultAuthenticator;
+
+const SCOPES: &[&str] = &["https://www.googleapis.com/auth/spreadsheets"];
+
+struct RestService {
+    client: Client,
+    auth: DefaultAuthenticator,
+}
+
+static SERVICE: OnceCell<RestService> = OnceCell::new();
+
+pub(crate) async fn initialize<'a>(key: &str) {
+    let client = Client::builder()
+        .timeout(std::time::Duration::from_secs(60))
+        .build()
+        .unwrap();
+    let auth = auth(key, SCOPES).await;
+    let inner = RestService { client, auth };
+    if SERVICE.set(inner).is_err() {
+        panic!(concat!("Already initialized sheets service"));
+    }
+}
 
 pub struct Range {
-    start: String,
-    end: String,
+    pub sheet: String,
+    pub start: String,
+    pub end: Option<String>,
 }
 
 impl Display for Range {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}:{}",
-            self.start.to_ascii_uppercase(),
-            self.end.to_ascii_uppercase()
-        )
+        if let Some(end) = &self.end {
+            write!(
+                f,
+                "'{}'!{}:{}",
+                self.sheet,
+                self.start.to_ascii_uppercase(),
+                end.to_ascii_uppercase()
+            )
+        } else {
+            write!(f, "'{}'!{}", self.sheet, self.start.to_ascii_uppercase(),)
+        }
     }
 }
 
@@ -72,6 +102,15 @@ impl Display for ValueRenderOption {
     }
 }
 
+impl Serialize for ValueRenderOption {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
 /// Determines how dates should be rendered in the output.
 pub enum DateTimeRenderOption {
     /// Instructs date, time, datetime, and duration fields to be output as
@@ -103,6 +142,15 @@ impl Default for DateTimeRenderOption {
     }
 }
 
+impl Serialize for DateTimeRenderOption {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
 /// Determines how input data should be interpreted.
 pub enum ValueInputOption {
     /// The values the user has entered will not be parsed and will be stored as-is.
@@ -129,6 +177,15 @@ impl Default for ValueInputOption {
     }
 }
 
+impl Serialize for ValueInputOption {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
 /// Determines how existing data is changed when new data is input.
 pub enum InsertDataOption {
     /// The new data overwrites existing data in the areas it is written.
@@ -152,14 +209,6 @@ impl Default for InsertDataOption {
     fn default() -> Self {
         InsertDataOption::Overwrite
     }
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum Value {
-    Number(f64),
-    Bool(bool),
-    String(String),
 }
 
 /// Data within a range of the spreadsheet.
@@ -192,7 +241,7 @@ pub struct ValueRange {
     /// For input, supported value types are: bool, string, and double.
     /// Null values will be skipped.
     /// To set a cell to an empty value, set the string value to an empty string.
-    pub values: Option<Vec<Vec<Option<Value>>>>,
+    pub values: Option<Vec<Vec<Option<String>>>>,
     /// The major dimension of the values.
     ///
     /// For output, if the spreadsheet data is: `A1=1,B1=2,A2=3,B2=4`,
@@ -209,9 +258,9 @@ pub struct ValueRange {
     pub major_dimension: Option<String>,
 }
 
-pub struct GetParams {
+pub struct GetParams<'a> {
     /// The ID of the spreadsheet to retrieve data from.
-    pub spreadsheet_id: String,
+    pub spreadsheet_id: &'a str,
     /// The A1 notation of the values to retrieve.
     pub range: Range,
     /// The major dimension that results should use.
@@ -232,7 +281,7 @@ pub struct GetParams {
 }
 
 /// Returns a range of values from a spreadsheet. The caller must specify the spreadsheet ID and a range.
-pub async fn get(params: GetParams) -> Result<ValueRange, String> {
+pub async fn get<'a>(params: GetParams<'_>) -> Result<ValueRange, String> {
     // GET https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{range}
     let mut query_params = Vec::with_capacity(6);
 
@@ -289,9 +338,9 @@ pub async fn get(params: GetParams) -> Result<ValueRange, String> {
     }
 }
 
-pub struct UpdateParams {
+pub struct UpdateParams<'a> {
     /// The ID of the spreadsheet to update.
-    pub spreadsheet_id: String,
+    pub spreadsheet_id: &'a str,
     /// The A1 notation of the values to update.
     pub range: Range,
     /// Data to upload.
@@ -342,7 +391,7 @@ pub struct UpdateValuesResponse {
 
 /// Sets values in a range of a spreadsheet. The caller must specify the
 /// spreadsheet ID, range, and a valueInputOption.
-pub async fn update(params: UpdateParams) -> Result<UpdateValuesResponse, String> {
+pub async fn update<'a>(params: UpdateParams<'_>) -> Result<UpdateValuesResponse, String> {
     // PUT https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{range}
     let mut query_params = Vec::with_capacity(6);
 
@@ -411,9 +460,9 @@ pub async fn update(params: UpdateParams) -> Result<UpdateValuesResponse, String
     }
 }
 
-pub struct AppendParams {
+pub struct AppendParams<'a> {
     /// The ID of the spreadsheet to update.
-    pub spreadsheet_id: String,
+    pub spreadsheet_id: &'a str,
     /// The A1 notation of a range to search for a logical table of data.
     /// Values are appended after the last row of the table.
     pub range: Range,
@@ -466,7 +515,7 @@ pub struct AppendValuesResponse {
 /// and a valueInputOption. The valueInputOption only controls
 /// how the input data will be added to the sheet (column-wise or row-wise),
 /// it does not influence what cell the data starts being written to.
-pub async fn append(params: AppendParams) -> Result<AppendValuesResponse, String> {
+pub async fn append<'a>(params: AppendParams<'_>) -> Result<AppendValuesResponse, String> {
     // POST https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{range}:append
     let mut query_params = Vec::with_capacity(6);
 
@@ -539,9 +588,9 @@ pub async fn append(params: AppendParams) -> Result<AppendValuesResponse, String
     }
 }
 
-pub struct BatchGetParams {
+pub struct BatchGetParams<'a> {
     /// The ID of the spreadsheet to retrieve data from.
-    pub spreadsheet_id: String,
+    pub spreadsheet_id: &'a str,
     /// The A1 notation of the values to retrieve.
     pub ranges: Vec<Range>,
     /// The major dimension that results should use.
@@ -579,7 +628,7 @@ pub struct BatchGetValuesResponse {
 
 /// Returns one or more ranges of values from a spreadsheet.
 /// The caller must specify the spreadsheet ID and one or more ranges.
-pub async fn batch_get(params: BatchGetParams) -> Result<BatchGetValuesResponse, String> {
+pub async fn batch_get<'a>(params: BatchGetParams<'_>) -> Result<BatchGetValuesResponse, String> {
     // GET https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values:batchGet
     let mut query_params = Vec::with_capacity(4 + params.ranges.len());
 
@@ -616,6 +665,140 @@ pub async fn batch_get(params: BatchGetParams) -> Result<BatchGetValuesResponse,
     let result = service
         .client
         .get(url)
+        .bearer_auth(token.as_str())
+        .send()
+        .await;
+
+    match result {
+        Ok(result) => {
+            if !result.status().is_success() {
+                return Err(format!(
+                    "Unexpected result with status code {}: {}",
+                    result.status(),
+                    result.text().await.unwrap()
+                ));
+            }
+            let result = result.json().await;
+            match result {
+                Ok(result) => Ok(result),
+                Err(err) => Err(err.to_string()),
+            }
+        }
+        Err(err) => Err(err.to_string()),
+    }
+}
+
+#[derive(Deserialize)]
+pub struct SheetProperties {
+    pub title: String,
+    // there are lot more but i skipped rest of fields
+    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/sheets#SheetProperties
+}
+
+#[derive(Deserialize)]
+pub struct Sheet {
+    pub properties: SheetProperties,
+    // there are lot more but i skipped rest of fields
+    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/sheets#Sheet
+}
+
+#[derive(Deserialize)]
+pub struct Spreadsheet {
+    pub sheets: Vec<Sheet>,
+    // there are lot more but i skipped rest of fields
+    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets#Spreadsheet
+}
+
+pub async fn get_spreadsheet_info(spreadsheet_id: &str) -> Result<Spreadsheet, String> {
+    // GET https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}
+    let url = format!(
+        "https://sheets.googleapis.com/v4/spreadsheets/{}",
+        spreadsheet_id
+    );
+    let url = reqwest::Url::parse(&url).unwrap();
+
+    let service = SERVICE.get().unwrap();
+    let token = service.auth.token(SCOPES).await.unwrap();
+
+    let result = service
+        .client
+        .get(url)
+        .bearer_auth(token.as_str())
+        .send()
+        .await;
+
+    match result {
+        Ok(result) => {
+            if !result.status().is_success() {
+                return Err(format!(
+                    "Unexpected result with status code {}: {}",
+                    result.status(),
+                    result.text().await.unwrap()
+                ));
+            }
+            let result = result.json().await;
+            match result {
+                Ok(result) => Ok(result),
+                Err(err) => Err(err.to_string()),
+            }
+        }
+        Err(err) => Err(err.to_string()),
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BatchUpdateParams<'a> {
+    /// The ID of the spreadsheet to update.
+    pub spreadsheet_id: &'a str,
+    /// How the input data should be interpreted.
+    pub value_input_option: Option<ValueInputOption>,
+    /// The new values to apply to the spreadsheet.
+    pub data: Vec<ValueRange>,
+    /// Determines if the update response should include the values of the cells that were updated.
+    /// By default, responses do not include the updated values.
+    /// The updatedData field within each of the BatchUpdateValuesResponse.responses
+    /// contains the updated values. If the range to write was larger than the range actually written,
+    /// the response includes all values in the requested range (excluding trailing empty rows and columns).
+    pub include_values_in_response: Option<bool>,
+    /// Determines how values in the response should be rendered.
+    /// The default render option is ValueRenderOption.FORMATTED_VALUE.
+    pub response_value_render_option: Option<ValueRenderOption>,
+    /// Determines how dates, times, and durations in the response should be rendered.
+    /// This is ignored if responseValueRenderOption is FORMATTED_VALUE.
+    /// The default dateTime render option is DateTimeRenderOption.SERIAL_NUMBER.
+    pub response_date_time_render_option: Option<DateTimeRenderOption>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct BatchUpdateResponse {
+    pub spreadsheet_id: Option<String>,
+    pub total_updated_rows: Option<usize>,
+    pub total_updated_columns: Option<usize>,
+    pub total_updated_cells: Option<usize>,
+    pub total_updated_sheets: Option<usize>,
+}
+
+/// Sets values in one or more ranges of a spreadsheet.
+/// The caller must specify the spreadsheet ID, a valueInputOption, and one or more ValueRanges.
+pub async fn batch_update<'a>(
+    params: BatchUpdateParams<'_>,
+) -> Result<BatchUpdateResponse, String> {
+    // POST https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values:batchUpdate
+    let url = format!(
+        "https://sheets.googleapis.com/v4/spreadsheets/{}/values:batchUpdate",
+        params.spreadsheet_id
+    );
+    let url = reqwest::Url::parse(&url).unwrap();
+
+    let service = SERVICE.get().unwrap();
+    let token = service.auth.token(SCOPES).await.unwrap();
+
+    let result = service
+        .client
+        .post(url)
+        .json(&params)
         .bearer_auth(token.as_str())
         .send()
         .await;
